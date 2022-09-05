3066d333d86a43a17765a05730fca65e33bccb9f
Moeen <moeensedaghaty86@gmail.com>
2022-09-05 11:42:09 +0430

    Linkedin Navbar Completed

--- quasar-first-profile/src/layouts/MainLayout.vue
+++ quasar-first-profile/src/layouts/MainLayout.vue
@@ -1,42 +1,80 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header>
-      <q-toolbar class="bg-primary text-white shadow-2">
-        <q-img
-          src="../assets/images/LinkedIn-Logo.png"
-          class="bg-white"
-          style="width: 24px; height: 24px"
-        />
+    <q-header class="shadow-2">
+      <div class="flex justify-center items-center">
+        <q-toolbar class="bg-primary text-white" style="width: 80%">
+          <q-img
+            src="../assets/images/LinkedIn-Logo.png"
+            class="bg-white"
+            style="width: 24px; height: 24px"
+          />
 
-        <q-input
-          dark
-          dense
-          standout
-          v-model="text"
-          input-class="text-right"
-          class="q-ml-md"
-        >
-          <template v-slot:append>
-            <q-icon v-if="text === ''" name="search" />
-            <q-icon
-              v-else
-              name="clear"
-              class="cursor-pointer"
-              @click="text = ''"
+          <q-input
+            dark
+            dense
+            standout
+            v-model="text"
+            input-class="text-right"
+            class="q-ml-md"
+          >
+            <template v-slot:append>
+              <q-icon v-if="text === ''" name="search" />
+              <q-icon
+                v-else
+                name="clear"
+                class="cursor-pointer"
+                @click="text = ''"
+              />
+            </template>
+          </q-input>
+
+          <q-space />
+
+          <q-tabs
+            v-model="tab"
+            class="text-white"
+            indicator-color="transparent"
+          >
+            <q-route-tab
+              to="notif"
+              name="mails"
+              icon="notifications"
+              label="Notifications"
             />
-          </template>
-        </q-input>
+            <q-tab to="apps" name="alarms" icon="apps" label="Work" />
+          </q-tabs>
 
-        <q-space />
+          <q-separator dark vertical />
 
-        <q-btn stretch flat label="Link" />
+          <q-item clickable v-ripple>
+            <q-item-section>
+              <div class="q-ml-md">
+                <q-item-label overline
+                  ><q-img
+                    src="../assets/images/LinkedIn-Logo.png"
+                    class="bg-white"
+                    style="width: 35px; height: 35px; border-radius: 50%"
+                /></q-item-label>
+                <q-item-label>Profile</q-item-label>
+              </div>
+            </q-item-section>
 
-        <q-btn stretch flat label="Link" />
-
-        <q-separator dark vertical />
-
-        <q-btn stretch flat label="Link" />
-      </q-toolbar>
+            <q-menu>
+              <q-list dense style="min-width: 100px">
+                <q-item
+                  v-for="n in 1"
+                  :key="n"
+                  dense
+                  clickable
+                  class="text-center"
+                >
+                  <q-item-section>Logout</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-item>
+        </q-toolbar>
+      </div>
     </q-header>
 
     <q-page-container>
@@ -46,13 +84,21 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "MainLayout",
-
+export default {
   setup() {
-    return {};
+    return {
+      text: ref(""),
+      tab: ref("mails"),
+    };
   },
-});
+};
 </script>
+
+<style>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
