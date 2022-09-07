c17114cb03f9d5999b2601159cdffef7de455d3b
Moeen <moeensedaghaty86@gmail.com>
2022-09-07 13:16:24 +0430

    Design Fixes

--- quasar-first-profile/src/pages/IndexPage.vue
+++ quasar-first-profile/src/pages/IndexPage.vue
@@ -26,7 +26,7 @@
       </q-bar>
     </div>
 
-    <!-- Gradient And Main Content -->
+    <!-- Gradient -->
 
     <div class="gradient full-width"></div>
 
@@ -39,23 +39,43 @@
             <!-- Profile Picture And Connections -->
 
             <div class="row justify-center items-center">
-              <div class="relative-position top--70 col-1">
-                <q-img
-                  class="shadow-3 q-mr-sm absolute"
-                  src="../assets/images/LinkedIn-Logo.png"
-                  width="95px"
-                  height="95px"
-                  style="border-radius: 50%"
-                  alt="Person"
-                />
+              <div class="gt-md">
+                <div class="relative-position top--70 col-1">
+                  <q-img
+                    class="shadow-3 q-mr-sm absolute"
+                    src="../assets/images/LinkedIn-Logo.png"
+                    width="95px"
+                    height="95px"
+                    style="border-radius: 50%"
+                    alt="Person"
+                  />
+                </div>
+                <div class="col-5">
+                  <span class="text-primary q-ml-xl q-pl-xl">+500</span
+                  ><span> Connections</span>
+                </div>
               </div>
-              <div class="col-5">
-                <span class="text-primary">+500</span><span> Connections</span>
+
+              <div class="lt-lg">
+                <div class="relative-position top--55 col-1">
+                  <q-img
+                    class="shadow-3 q-mr-sm absolute"
+                    src="../assets/images/LinkedIn-Logo.png"
+                    width="95px"
+                    height="95px"
+                    style="border-radius: 50%"
+                    alt="Person"
+                  />
+                </div>
+                <div class="col-5">
+                  <span class="text-primary q-ml-xl q-pl-xl">+500</span
+                  ><span> Connections</span>
+                </div>
               </div>
 
               <q-space />
 
-              <div class="q-mt-md">
+              <div class="q-mt-md gt-md">
                 <q-btn-dropdown
                   dropdown-icon="more_horiz"
                   class="q-px-sm q-mx-sm col-6"
@@ -80,11 +100,39 @@
                   label="Connect"
                 />
               </div>
+
+              <div class="q-mt-md lt-lg">
+                <div class="q-mt-md row justify-center align-center">
+                  <q-btn-dropdown
+                    dropdown-icon="more_horiz"
+                    class="q-px-sm q-mx-sm"
+                    no-icon-animation
+                  >
+                    <q-list>
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Photos</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-btn-dropdown>
+                  <q-btn
+                    class="q-px-sm q-mx-sm"
+                    icon="mark_email_read"
+                    label="Message"
+                  />
+                  <q-btn
+                    class="bg-primary q-px-sm text-white q-mx-sm q-mt-sm"
+                    icon="add"
+                    label="Connect"
+                  />
+                </div>
+              </div>
             </div>
 
             <!-- Profile -->
 
-            <div class="q-pt-md row">
+            <div class="q-pt-md row gt-md">
               <div class="col-8">
                 <div class="text-h5 text-weight-bold">Moeen Sedaqati</div>
                 <div>
@@ -137,6 +185,59 @@
                 </div>
               </div>
             </div>
+
+            <div class="q-pt-md lt-lg">
+              <div>
+                <div class="text-h5 text-weight-bold">Moeen Sedaqati</div>
+                <div>
+                  Quasar, Vue, Laravel, Html, Css, Wordpress, Figma - Workin At
+                  <a
+                    href="https://aytronic.com/"
+                    class="text-primary text-weight-bold"
+                    >Aytronic</a
+                  >
+                </div>
+                <div class="text-grey-6">Iran Golestan, Gonbad-E Qabus</div>
+                <div>
+                  <q-img
+                    src="../assets/images/LinkedIn-Logo.png"
+                    width="30px"
+                    height="30px"
+                    style="border-radius: 50%"
+                  />
+                  <span
+                    >1 Mutual Connection:
+                    <span class="text-grey-6">Orlando Diggs</span></span
+                  >
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <div>
+                    <q-img
+                      src="../assets/images/LinkedIn-Logo.png"
+                      class="q-px-md"
+                      width="30px"
+                      height="30px"
+                      style="border-radius: 50%"
+                    />
+                    <span>Notion</span>
+                  </div>
+
+                  <div>
+                    <q-img
+                      src="../assets/images/LinkedIn-Logo.png"
+                      class="q-px-md"
+                      width="30px"
+                      height="30px"
+                      style="border-radius: 50%"
+                    />
+                    <span>Harvard University</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
@@ -145,8 +246,8 @@
         <!-- About -->
 
         <div class="row">
-          <div class="col-6">
-            <q-toolbar>
+          <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+            <q-toolbar class="p-none">
               <q-toolbar-title class="text-weight-bold">
                 About
               </q-toolbar-title>
@@ -158,16 +259,14 @@
             </q-toolbar>
             <div>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Consequatur rerum unde incidunt id, autem pariatur, voluptate aut
-              porro itaque ducimus commodi, illo amet quas voluptatibus ea. Eum
-              quod nemo tempora?
+              Consequatur rerum unde incidunt id, autem pariatur
             </div>
 
             <q-separator class="q-my-lg" />
 
             <div class="text-weight-bold text-h6">Experience</div>
 
-            <q-card class="my-card" flat>
+            <q-card class="my-card gt-md" flat>
               <q-card-section horizontal>
                 <q-card-section>
                   <q-avatar>
@@ -188,14 +287,36 @@
                 </q-card-section>
               </q-card-section>
             </q-card>
+
+            <q-card class="my-card lt-lg" flat>
+              <q-card-section horizontal>
+                <q-card-section>
+                  <q-avatar>
+                    <img src="../assets/images/Aytronic-logo.png" />
+                  </q-avatar>
+                </q-card-section>
+                <q-card-section class="q-pt-xs">
+                  <div class="text-h5 q-mt-sm q-mb-xs">
+                    <a href="http://aytronic.com">Aytronic</a>
+                  </div>
+                  <div class="text-grey">Part-Time</div>
+                  <div class="text-grey">3 Month</div>
+                  <div class="text-caption text-grey">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </div>
+                </q-card-section>
+              </q-card-section>
+            </q-card>
             <q-separator class="q-my-md" />
           </div>
 
-          <q-separator vertical class="q-mx-lg" />
+          <q-separator vertical class="q-mx-lg gt-md" />
 
           <!-- People Viewed This Website -->
 
-          <div class="col-5">
+          <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <div class="text-weight-bolder q-ml-md">People Also Viewed</div>
             <q-item class="q-my-xs">
               <q-item-section avatar>
@@ -314,7 +435,11 @@ export default defineComponent({
   top: -70px;
 }
 
-.q-toolbar {
+.top--55 {
+  top: -55px;
+}
+
+.p-none {
   padding: 0;
 }
 
